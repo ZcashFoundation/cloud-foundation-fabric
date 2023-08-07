83ebcffbd904dc17ffdd25180e8141a7677126f4
Gustavo Valverde <gustavo@iterativo.do>
2023-08-07 17:50:43 +0100

    feat(dev): add `zfnd` organization rules and `zfnd-dev-zebra` project

--- fast/stages/3-project-factory/dev/main.tf
+++ fast/stages/3-project-factory/dev/main.tf
@@ -36,4 +36,87 @@ module "projects" {
   factories_config = var.factories_config
 }
 
+module "zebra_caching_artifact_registry" {
+  source      = "../../../../modules/artifact-registry"
+  project_id  = "zfnd-dev-zebra"
+  location    = "us"
+  format      = "DOCKER"
+  id          = "zebra"
+  description = "Docker repository storing the Zebra application for testing purposes"
+  iam = {
+    "roles/artifactregistry.reader" = ["allUsers"]
+  }
+}
 
+module "zebra_artifact_registry" {
+  source      = "../../../../modules/artifact-registry"
+  project_id  = "zfnd-dev-zebra"
+  location    = "us"
+  format      = "DOCKER"
+  id          = "zebra-caching"
+  description = "Docker repository storing Zebra's build layers for caching"
+  iam = {
+    "roles/artifactregistry.reader" = ["allUsers"]
+  }
+}
+
+module "lwd_caching_artifact_registry" {
+  source      = "../../../../modules/artifact-registry"
+  project_id  = "zfnd-dev-zebra"
+  location    = "us"
+  format      = "DOCKER"
+  id          = "lightwalletd"
+  description = "Docker repository storing the Zebra application for testing purposes"
+  iam = {
+    "roles/artifactregistry.reader" = ["allUsers"]
+  }
+}
+
+module "lwd_artifact_registry" {
+  source      = "../../../../modules/artifact-registry"
+  project_id  = "zfnd-dev-zebra"
+  location    = "us"
+  format      = "DOCKER"
+  id          = "lightwalletd-caching"
+  description = "Docker repository storing Zebra's build layers for caching"
+  iam = {
+    "roles/artifactregistry.reader" = ["allUsers"]
+  }
+}
+
+resource "google_compute_health_check" "http-health-check" {
+  name        = "zebrad-tracing-filter"
+  description = "Health check via http"
+  project     = "zfnd-dev-zebra"
+
+  timeout_sec         = 10
+  check_interval_sec  = 30
+  healthy_threshold   = 2
+  unhealthy_threshold = 3
+
+  http_health_check {
+    port               = "3000"
+    port_specification = "USE_FIXED_PORT"
+    request_path       = "/filter"
+    proxy_header       = "NONE"
+    # TODO: we should validate a specific response, not ANY response
+    # response           = "I AM HEALTHY"
+  }
+}
+
+module "runner-mig-dind" {
+  source         = "github.com/terraform-google-modules/terraform-google-github-actions-runners?ref=v3.1.1//modules/gh-runner-mig-container-vm"
+  create_network = true
+  subnetwork_project = var.subnetwork_project
+  subnet_name    = var.subnet_name
+  subnet_ip     = var.subnet_ip
+  service_account = null
+  project_id     = var.project_id
+  image          = var.image
+  repo_name      = var.repo_name
+  repo_url       = var.repo_url
+  repo_owner     = var.repo_owner
+  gh_token       = var.gh_token
+  region         = var.region
+  dind           = true
+}
--- fast/stages/3-project-factory/dev/outputs.tf
+++ fast/stages/3-project-factory/dev/outputs.tf
@@ -28,3 +28,15 @@ output "service_accounts" {
   description = "Created service accounts."
   value       = module.projects.service_accounts
 }
+
+# self-hosted runners outputs
+
+output "mig_instance_group" {
+  description = "The instance group url of the created MIG"
+  value       = module.runner-mig-dind.mig_instance_group
+}
+
+output "mig_name" {
+  description = "The name of the MIG"
+  value       = module.runner-mig-dind.mig_name
+}
